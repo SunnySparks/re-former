64ca4af5627398603a9d5200a2e947ef3fffc040
SunnySparks <josediazt6@outlook.com>
2021-01-20 16:56:47 -0600

    Create form skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :user,only: [:new, :create]
+  get 'users/:new', to: 'users#new'
+  
+  resources :user, only: [:new, :create]
 end
