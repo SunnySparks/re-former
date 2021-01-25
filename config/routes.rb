72d6fc9a63f548d6b88455aa202e6e5e22cdda22
SunnySparks <josediazt6@outlook.com>
2021-01-25 14:54:04 -0600

    Fix minor issues from TSE

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "users/new" => "users#new", :as => :new_user
-  post "users/new" => "users#new"
-
-  get 'users/edit', to: 'users#edit', as: :edit_user
-  patch 'users/:id', to: 'users#update'
-
   resources :users, only: [:new, :create, :edit, :update]
 end
