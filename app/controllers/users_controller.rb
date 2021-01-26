dc9758f05b4911a99ab0669f34f8fa6e5fa4ffe0
SunnySparks <josediazt6@outlook.com>
2021-01-26 15:35:55 -0600

    Fix editing issue
    The application can now edit the database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,23 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id].to_i)
   end
 
   def update
+    @user = User.update(user_params)
+    if User.update(user_params)
+      redirect_to new_user_path notice: 'User successfully updated'
+    else
+      render :edit
+    end
   end
 
   private
+
+  def find_params
+    params.require(:email)
+  end
 
   def user_params
     params.require(:user).permit(:email, :password, :username)
