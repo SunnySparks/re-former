9a399ef8d31725e78499d8539bc10934f853e830
SunnySparks <josediazt6@outlook.com>
2021-01-26 16:35:35 -0600

    Edit users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,39 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
 
-    def new
-        @user = User.new
+  def create
+    @user = User.new(user_params)
+    # @user = User.new(user: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
+  end
 
-    def create
-        @user = User.new(user_params)
-        #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
-      
-        if @user.save
-          redirect_to new_user_path
-        else
-          render :new
-        end
+  def edit
+    @user = User.find(params[:id].to_i)
+  end
+
+  def update
+    @user = User.update(user_params)
+    if User.update(user_params)
+      redirect_to new_user_path notice: 'User successfully updated'
+    else
+      render :edit
     end
+  end
 
-    def edit
-    end
+  private
 
-    private
-    def user_params
-        params.require(:user).permit(:email, :password, :username)
-    end
+  def find_params
+    params.require(:email)
+  end
 
+  def user_params
+    params.require(:user).permit(:email, :password, :username)
+  end
 end
